739552b3ddd15413e912465772a64916bdfffe89
Donato Meoli <donato.meoli.95@gmail.com>
2026-09-01 21:17:26 +0200

    the Solution version of is_feasible() and is_optimal() has a name of its own, so overriding one does not hide the other

--- include/InvestmentBlock.h
+++ include/InvestmentBlock.h
@@ -547,8 +547,6 @@ public:
  bool is_feasible( bool useabstract = false ,
                    Configuration * fsbc = nullptr ) override;
 
- /// the Solution version is not hidden by the one above [see Block]
- using Block::is_feasible;
 
 /**@} ----------------------------------------------------------------------*/
 /*--------------------- PROTECTED PART OF THE CLASS ------------------------*/
